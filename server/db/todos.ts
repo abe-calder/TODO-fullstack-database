52da65aefe167e52e312b071aebdc9796b49ff40
Abe <214738266+abe-calder@users.noreply.github.com>
2025-08-26 10:57:30 +1200

    create addTodo function

--- server/db/todos.ts
+++ server/db/todos.ts
@@ -3,3 +3,18 @@ import db from './connection'
 export async function getAllTodos() {
   return db('todos').select()
 }
+
+export async function addTodo(newTodo: {
+  task: string
+  person_name: string
+  responsibilities: string
+  deadline: string
+  is_done: boolean
+}) {
+  try {
+    await db('todos').insert(newTodo).returning('*')
+  } catch (error) {
+    console.error(error)
+    console.log(error)
+  }
+}
