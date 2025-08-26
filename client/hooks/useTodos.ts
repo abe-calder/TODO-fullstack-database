0784b738c9a79ce58af452d6b9c1d8c3bb66a2b6
Abe <214738266+abe-calder@users.noreply.github.com>
2025-08-26 16:28:04 +1200

    start use hooks

---
+++ client/hooks/useTodos.ts
@@ -0,0 +1,27 @@
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  MutationFunction
+} from '@tanstack/react-query'
+
+import { getTodos, addTodo, deleteTodo } from '../apis/todos'
+
+export function useTodos() {
+  const query = useQuery({ queryKey: ['todos'], queryFn: getTodos })
+  return {
+    ...query
+  }
+}
+
+export function useTodosMutation (
+  mutationFn: MutationFunction,
+) {
+  const queryClient = useQueryClient()
+  const mutation = useMutation({
+    mutationFn,
+    onSuccess: () => {
+      queryClient
+    }
+  })
+}
